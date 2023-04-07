babe9b5888b84e7320a0a5ac0953334b5f2b7d83
niszco <nico.marszalek2@gmail.com>
2023-04-07 15:10:19 +0200

    Add script SQL

---
+++ scriptSQL/script.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Utilisateur (
+    id_utilisateur INT NOT NULL PRIMARY KEY AUTO INCREMENT,
+    login_utilisateur VARCHAR(25) NOT NULL,
+    pwd_utilisateur VARBINARY(255) NOT NULL,
+    id_categorie_utilisateur int REFERENCES CategorieUtilisateur(id_categorie_utilisateur) NOT NULL
+);
+
+CREATE TABLE CategorieUtilisateur (
+    id_categorie_utilisateur INT NOT NULL PRIMARY KEY AUTO INCREMENT,
+    nom_categorie VARCHAR(50) NOT NULL
+);
