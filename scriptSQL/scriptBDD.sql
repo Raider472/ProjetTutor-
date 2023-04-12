6801626f92710539bcc3f14082f28c0fbb959678
niszco <nico.marszalek2@gmail.com>
2023-04-12 19:59:01 +0200

    🚀 (BDD) Ajouts jeu de données

--- scriptSQL/scriptBDD.sql
+++ scriptSQL/scriptBDD.sql
@@ -90,14 +90,14 @@ CREATE TABLE Historique (
 
 -- Création du jeu de données;
 
-INSERT INTO Utilisateur VALUES (1, 'sudo', 'sudo', 1), (2, 'admin', 'admin', 2), (3, 'pgeorges', 'Georges84@'), (4, 'nmart', 'nat78#');
-INSERT INTO CategorieUtilisateur VALUES (1, 'super_admin'),(2, 'admin'),(3, 'opérateur'),(4,'utilisateur');
+INSERT INTO Utilisateur VALUES (1, 'sudo', AES_ENCRYPT('sudo'), 1), (2, 'admin', AES_ENCRYPT('admin'), 2), (3, 'operatortest', AES_ENCRYPT('operatortest'), 3), (4, 'usertest', AES_ENCRYPT('usertest'), 4), (5, 'pgeorges', AES_ENCRYPT('Georges84@'), 3), (6, 'nmart', AES_ENCRYPT('nat78#'), 4);
+INSERT INTO CategorieUtilisateur VALUES (1, 'super_admin'), (2, 'admin'), (3, 'opérateur'), (4,'utilisateur');
 INSERT INTO Playlist VALUES (1, '2022-03-10', '2022-03-17', 'Outilages', '2022-03-07 15:47:12'),(2, '2022-03-23', '2022-03-30', 'Maroquinerie', '2022-03-19 11:23:43');
-INSERT INTO Pubs VALUES (1, 'Nouveau sac 1', 75.00, '.mp4'),(2, 'Sets tournevis 1', 43.34, '.mkv'),(3, 'Nouveau jouet 1', 0, '.png'),(4, 'Promotion Fromagerie', 0, '.jpeg');
-INSERT INTO Terminal VALUES (1, 'Samnsung modèle X', '00:1P:56:11:3B:L7', 'Écran de télévision', 'Outilages', 'Outilages'),(2, 'Samnsung modèle X', '01:3R:34:67:9Y:J0', 'Écran de télévision', 'Fromagerie', 'Fromagerie');
-INSERT INTO TypeTerminal VALUES (1, 'Écran de télévision'), (2, 'Écran de télévision');
-INSERT INTO EmplacementTerminal VALUES (1, 'Fromagerie'),(2, 'Outilages'),(3, 'Boucherie'),(4, 'Prêt-à-porter'),(5, 'Poissonnerie');
-INSERT INTO PlaylistPubs VALUES (1, 2, 1);
-INSERT INTO PlaylistTerminaux VALUES (1, 1, 1);
-INSERT INTO TypeFichier VALUES ('.mp3', 'MPEG-2'),('.mp4', 'MPEG-4'),('.mkv', 'Matroska Video'),('.png', 'Portable Network Graphic'),('.jpeg', 'Joint Photographic Experts Group'),('.gif', 'Graphics Interchange Format');
-INSERT INTO Historique VALUES (1, 1, 1, '2022-03-10', '2022-03-17', 1, 'Écran de télévision', 3);
+INSERT INTO Pubs VALUES (1, 'Nouveau sac 1', 75.00, '.mp4'), (2, 'Sets tournevis 1', 43.34, '.mkv'), (3, 'Nouveau jouet 1', 0, '.png'), (4, 'Promotion Fromagerie', 0, '.jpeg'), (5, 'Promotion Boucherie', 0, '.gif');
+INSERT INTO Terminal VALUES (1, 'Samsung modèle X', '00:1P:56:11:3B:L7', 'Écran de télévision', 'Outilages', 'Outilages'), (2, 'Samsung modèle X', '01:3R:34:67:9Y:J0', 'Écran de télévision', 'Fromagerie', 'Fromagerie'), (2, 'Samsung modèle X', '78:3K:89:32:5T:B5', 'Écran de télévision', 'Maroquinerie', 'Maroquinerie');
+INSERT INTO TypeTerminal VALUES (1, 'Écran de télévision'), (2, 'Ordinateur'), (3, 'Smartphone'), (4, 'Autres');
+INSERT INTO EmplacementTerminal VALUES (1, 'Fromagerie'), (2, 'Outilages'), (3, 'Boucherie'), (4, 'Prêt-à-porter'), (5, 'Poissonnerie'), (6, 'Boulangerie'), (7, 'Sports'), (8, 'Multimédia');
+INSERT INTO PlaylistPubs VALUES (2, 1), (1, 2);
+INSERT INTO PlaylistTerminaux VALUES (1, 1), (2, 3);
+INSERT INTO TypeFichier VALUES ('.mp3', 'MPEG-2'), ('.mp4', 'MPEG-4'), ('.mkv', 'Matroska Video'), ('.png', 'Portable Network Graphic'), ('.jpeg', 'Joint Photographic Experts Group'), ('.gif', 'Graphics Interchange Format');
+INSERT INTO Historique VALUES (1, 1, 1, '2022-03-10', '2022-03-17', 1, 'Écran de télévision', 3078), (2, 2, 2, '2022-12-13', '2022-12-29', 3, 'Écran de télévision', 28890);
