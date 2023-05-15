73d29b0c59941fe974cf555eb9ab86a7d18a9e20
Raider <gabrielnae1@gmail.com>
2023-05-15 16:13:53 +0200

    Svelte + API

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     require_once("PHP/DAO/UtilisateursDAO.php");
     $DAO = new LesUtilisateurs();
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let url = "http://localhost:8100/"
+    function RecevoirJson() {
+        fetch(url)
+            .then(function (response) {
+                if(response.ok) {
+                    return response.json()
+                }
+                else {
+                    alert("problème")
+                }
+            })
+            .catch(function (erreur) {
+                alert(erreur.message)
+            })
+            .then(function (utilisateur) {
+                console.log(utilisateur)
+            })
+    }
+</script>
+
+<h1>Test API</h1>
+<section>
+    <button on:click={RecevoirJson}>
+        Reçevoir données
+    </button>
+    <button>
+        Envoyer Données
+    </button>
+    <input type="text">
+</section>
