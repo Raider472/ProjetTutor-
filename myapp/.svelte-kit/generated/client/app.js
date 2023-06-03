6e52d106fa37f0176d86ed6e63e9003c5bf11f02
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-06-03 15:05:57 +0200

    Ajout d'une page pour l'ajout d'une pub + Modif de routes pour test + Ajout de fichiers dans le .gitignore

--- myapp/.svelte-kit/generated/client/app.js
+++ myapp/.svelte-kit/generated/client/app.js
@@ -5,7 +5,8 @@ export const nodes = [
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
-	() => import('./nodes/4')
+	() => import('./nodes/4'),
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
@@ -13,7 +14,8 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/accueil": [3],
-		"/interfacePub": [4]
+		"/creationPub": [4],
+		"/interfacePub": [5]
 	};
 
 export const hooks = {
---
+++ myapp/src/routes/creationPub/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+
+
+
+
+
+
+</script>
+
+<body>
+    <header>Ajout d'une playlist</header>
+    <form id="pubForm">
+        <label for="urlVideoInput">Vidéo par URL Internet</label>
+        <input type="text" id="urlInput" name="urlVideoInput">
+        <label for="pubsdispInput">Contenu disponible dans la base de données</label>
+        <input type="text" id="pubsdispInput" name="pubsdispInput">
+        <table>
+            <tr>
+              <th>Colonne 1</th>
+              <th>Colonne 2</th>
+              <th>Colonne 3</th>
+            </tr>
+            <tr>
+              <td>Ligne 1, Cellule 1</td>
+              <td>Ligne 1, Cellule 2</td>
+              <td>Ligne 1, Cellule 3</td>
+            </tr>
+            <tr>
+              <td>Ligne 2, Cellule 1</td>
+              <td>Ligne 2, Cellule 2</td>
+              <td>Ligne 2, Cellule 3</td>
+            </tr>
+            <tr>
+              <td>Ligne 3, Cellule 1</td>
+              <td>Ligne 3, Cellule 2</td>
+              <td>Ligne 3, Cellule 3</td>
+            </tr>
+            <tr>
+              <td>Ligne 4, Cellule 1</td>
+              <td>Ligne 4, Cellule 2</td>
+              <td>Ligne 4, Cellule 3</td>
+            </tr>
+          </table>
+
+    </form>
+    
+</body>
+
+
+<style>
+    body {
+        background-color: #9B9B9B;
+        font-family: 'Roboto', sans-serif;
+    }
+    header {
+        text-align: center;
+        padding-bottom: 10%;
+    }
+    table,td {
+        background: #E3E3E3;
+        border: 1px solid #333;
+    }
+    
+</style>
