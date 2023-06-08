829c39908d92003e5749f566aeb35617fdaea6ae
Raider <gabrielnae1@gmail.com>
2023-06-08 18:27:32 +0200

    accueil Abou

--- front/src/routes/accueil/+page.svelte
+++ front/src/routes/accueil/+page.svelte
@@ -1,22 +1,36 @@
-<title>Title</title>
+<title>Accueil</title>
 
 <div id="pageAc">
     <div class="parent">
         <div class="entete-gauche">
-            PlaylistAPI
+            NOM APP
         </div>
         <div class="entete-droit">Bienvenue</div>
         <div class="corps-gauche">
+            <input id="btn_playlist" type="button" value="Playlists">
+            <br>
+            <input id="btn_utilisateurs" type="button" value="Utilisateurs">
+            <br>
+            <input id="btn_terminaux" type="button" value="Terminaux">
+            <br>
+            <br>
+            <input id="btn_creer" type="button" value="crÃ©er">
+            <br>
+            <input id="btn_supprimer" type="button" value="supprimer">
+            <br>
+            <input id="btn_modifier" type="button" value="modifier">
+            <br>
+            <input type="button" value="Historique">
+
         </div>
         <div class="corps-droit"></div>
     </div>
 </div>
 
 <style>
-
     .parent {
         display: grid;
-        grid-template-columns: repeat(8, 1fr);
+        grid-template-columns: repeat(10, 1fr);
         grid-template-rows: repeat(5, 1fr);
         grid-column-gap: 0px;
         grid-row-gap: 0px;
@@ -26,16 +40,12 @@
     .entete-gauche {
         grid-area: 1 / 1 / 2 / 3;
         background-color: #3c3c3c;
-        text-align: center;
-        font-size: 4em;
         color: white;
     }
 
     .entete-droit {
         grid-area: 1 / 3 / 2 / 9;
         background-color: #888888;
-        text-align: center;
-        font-size: 7em;
         color: white;
     }
 
